e254912b9b7491f8e522f7447d3a6943cd1d8d93
PatilAarya <patilaarya1414@gmail.com>
2026-08-08 09:09:36 +0530

    Time: 1292 ms (80.53%), Space: 0B (100%) - LeetHub

---
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -0,0 +1,20 @@
+(SELECT name AS results
+FROM Users u
+JOIN MovieRating mr
+ON u.user_id = mr.user_id
+GROUP BY u.user_id, u.name
+ORDER BY COUNT(*) DESC, u.name ASC
+LIMIT 1)
+
+UNION ALL
+
+(SELECT title AS results
+FROM Movies m
+JOIN MovieRating mr
+ON m.movie_id = mr.movie_id
+WHERE created_at >= '2020-02-01'
+AND created_at < '2020-03-01'
+GROUP BY m.movie_id, m.title
+ORDER BY AVG(rating) DESC, m.title ASC
+LIMIT 1
+)
